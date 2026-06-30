9d1490093959592280e0a9b864cb06e3e6e5b916
elhafsirayen <elhafsirayen@gmail.com>
2026-06-30 19:13:48 +0100

    changes

--- lib/screens/owner_home.dart
+++ lib/screens/owner_home.dart
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
